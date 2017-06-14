7a78d7d092e827d09d3bce91bf9a869f9ce800e5
kamiyayasuto <y.kamiya.201604@gmail.com>
2017-06-14 11:40:13 -0700

    re12

--- db/schema.rb
+++ db/schema.rb
@@ -48,13 +48,6 @@ ActiveRecord::Schema.define(version: 20170614145533) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "pictures", force: :cascade do |t|
-    t.string   "title"
-    t.binary   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
